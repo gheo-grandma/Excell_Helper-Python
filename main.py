e357144870a93b920b40ac836339d0b44e415303
gheo-grandma <bonetto.matteof@gmail.com>
2024-03-28 21:13:22 +0100

    feat: aggiunto pseudo codice per la lettura del file excel'

--- main.py
+++ main.py
@@ -1,38 +1,51 @@
 import openpyxl                             # modulo per leggere excel .xlsx (solo questo formato!)
 import dates_operations  as do              # script esterno creato per gestire le date
-from smtplib import SMTP                    # modulo per email
-
-
-
-def login_to_mail(body):
-    
-    # address, port number
-    # questi valori cambiano insieme in base al servizio che viene usato
-    server = SMTP('smtp.gmail.com', 587)
-    
-    
-    # creao una connessione sicura con SSL o TLS
-    server.starttls()
-    
-    # username, password
-    server.login('bonetto.matteof@gmail.com', 'uojtdczjuoetpuxg')
-    
-    # sender, receiver, message
-    server.sendmail('matthew.bonetto@gmail.com', 'bonetto.matteof@gmail.com', body)
-    
-    print('Mail sent')
+import mail                                 # script esterno creato per login ed invio della email
 
 
 
 if __name__ == "__main__":
     
-
+    # ricevo la data formattata tramite lo script
     today = do.get_today_date()
-    print(f'Oggi è il {today}.')   
+    print(f'Oggi è il {today}.')  
+    
+    # inserisco la data da cui calcolare i giorni rimasti 
     expiration = input('expire dd/mm/yyyy: ')  
+    
     
     difference_between_dates = do.days(expiration) - do.days(today)
     
-    login_to_mail(f'Ciao vecio! mancano {difference_between_dates} giorni a {expiration}.')
+    # invio la email 
+    mail.send_mail(f'Ciao vecio! mancano {difference_between_dates} giorni a {expiration}.')
     
     print(f'tot is {difference_between_dates}')
+    
+    
+    # cosa resa da fare: implementare la lettura dal file xlsx
+    # eseguire lo script in questo modo:
+    # creao un dizionario: {nome_dipendente : {nome_del_corso : giorni rimasti alla scadeza}}
+    # esempio:
+    corsi_in_scadenza = {
+        'Matteo Bonetto' : {
+            'transpallet elettrico' : 126,
+            'scaffali' : 154,
+            'cassa' : 67
+        },
+        
+        'Eleonora Mori' : {
+            'scaffali' : 154,
+            'sicurezza' : 139
+        } 
+    }
+    
+    # il nome del dipendente diventa la chiave primaria del dizionario dei dipendenti
+    # ad ogni dipendente viene associato un dizionario che ha come chiave il nome del corso e come valore i giorni rimanenti alla scadenza del corso
+    # il corpo della mail viene formattato così: nome del dipendente, li sta dei corsi in scadenza
+    # esempio:
+    
+    messaggio_esempio = 'Sono in scadenza per Matteo Bonetto i corsi transpallet elettrico (126 giorni), scaffali (154 giorni), cassa (67 giorni).\n Sono in scadenza per Eleonora Mori i corsi scaffali (154 giorni), sicurezza (139 giorni)'
+    
+    # nella funzione per il controllo dei giorni in scadenza è peresente un dizionario di cache per ottimizzare la memoria:
+    # quando viene riscontrata una data, se non è presente nella cache viene inserita come chiave, mentre il suo valore è il risultato del calcolo
+    # il risultato del calcolo viene già inserito nel dizionario dei dipendenti
