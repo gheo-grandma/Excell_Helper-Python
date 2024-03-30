86301f25e198b794e67235053f866cf37bb386c3
gheo-grandma <bonetto.matteof@gmail.com>
2024-03-30 14:20:09 +0100

    feat: mail format

--- mail.py
+++ mail.py
@@ -1,7 +1,24 @@
 from smtplib import SMTP                    # modulo per email
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 def send(body):
+    
+    sender_email = 'matthew.bonetto@gmail.com'
+    receiver_email = 'bonetto.matteof@gmail.com'
+    
+    # Create the email message
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = 'Corsi in scadenza'
+    
+    # Attach the email body
+    msg.attach(MIMEText(body, 'plain'))
+
+    # Convert the email body to a string
+    email_text = msg.as_string()
     
     # address, port number
     # questi valori cambiano insieme in base al servizio che viene usato
@@ -12,9 +29,12 @@ def send(body):
     server.starttls()
     
     # username, password ( la password non è la stessa dell'account, bensì la password per le app terze parti generata apposta da google )
-    server.login('bonetto.matteof@gmail.com', 'uojtdczjuoetpuxg')
+    server.login(receiver_email, 'uojtdczjuoetpuxg')
     
     # sender, receiver, message
-    server.sendmail('matthew.bonetto@gmail.com', 'bonetto.matteof@gmail.com', body)
+    server.sendmail(sender_email, receiver_email, email_text)
     
+    
+    
+    server.quit()
     print('Mail sent')
--- main.py
+++ main.py
@@ -40,29 +40,29 @@ if __name__ == "__main__":
             'scaffali' : 154,
             'sicurezza' : 139
         } 
-    }
+    } 
     
     # il nome del dipendente diventa la chiave primaria del dizionario dei dipendenti
     # ad ogni dipendente viene associato un dizionario che ha come chiave il nome del corso e come valore i giorni rimanenti alla scadenza del corso
     # il corpo della mail viene formattato così: nome del dipendente, li sta dei corsi in scadenza
     # esempio:
     
-    messaggio_esempio = 'Sono in scadenza per Matteo Bonetto i corsi transpallet elettrico (126 giorni), scaffali (154 giorni), cassa (67 giorni).\n Sono in scadenza per Eleonora Mori i corsi scaffali (154 giorni), sicurezza (139 giorni)'
+    messaggio_esempio = f'Sono in scadenza per Matteo Bonetto i corsi transpallet elettrico (126 giorni), scaffali (154 giorni), cassa (67 giorni).\nSono in scadenza per Eleonora Mori i corsi scaffali (154 giorni), sicurezza (139 giorni)'
     
     # nella funzione per il controllo dei giorni in scadenza è peresente un dizionario di cache per ottimizzare la memoria:
     # quando viene riscontrata una data, se non è presente nella cache viene inserita come chiave, mentre il suo valore è il risultato del calcolo
     # il risultato del calcolo viene già inserito nel dizionario dei dipendenti
     
     
-    output = ""
+    output = "Buongiorno Eleonora ❤️ Sono in scadenza i seguenti corsi per i seguenti dipendenti:\n"
     for nomi, corsi in corsi_in_scadenza.items():
-        output += f'Sono in scadenza per {nomi} i corsi: '
+        output += f'- {nomi}: '
         
         for corso, giorni in corsi.items():
-            output += f'{corso} ({giorni} giorni), '
+            output += f'{corso} ({giorni} giorni alla scadenza), '
             
         output = output[:-2]
-        output += f'. '
+        output += f'.\n'
         
     
         
